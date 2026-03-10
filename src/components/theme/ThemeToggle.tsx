a3c0266969529455aac8e20b5746ca99eaba3082
Moe Youssef <moyoussef182005@gmail.com>
2026-03-10 10:16:17 +0200

    Add new dynamic page component and enhance ThemeToggle with keyboard support
    - Created a new dynamic page component in `src/app/(protected)/[...slug]/page.tsx` to display content based on URL slug.
    - Enhanced `ThemeToggle` component to include keyboard shortcut support for toggling themes using the 'D' key.
    - Integrated tooltip functionality in `ThemeToggle` for better user experience.

--- src/components/theme/ThemeToggle.tsx
+++ src/components/theme/ThemeToggle.tsx
@@ -5,10 +5,20 @@ import { Button } from "../ui/button";
 import { useTheme } from "next-themes";
 import { IconBrightness } from "@tabler/icons-react";
 import { Skeleton } from "../ui/skeleton";
+import { useHotkey } from "@tanstack/react-hotkeys";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+import { Kbd } from "../ui/kbd";
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = React.useState(false);
+
+  const handleThemeToggle = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  useHotkey("D", handleThemeToggle);
+
   React.useEffect(() => {
     setMounted(true);
   }, []);
@@ -17,15 +27,25 @@ export default function ThemeToggle() {
     return <Skeleton className="size-8" />;
   }
 
-  const handleThemeToggle = () => {
-    setTheme(theme === "dark" ? "light" : "dark");
-  };
-
   return (
     <>
-      <Button size={"icon"} variant={"ghost"} onClick={handleThemeToggle}>
-        <IconBrightness />
-      </Button>
+      <Tooltip>
+        <TooltipTrigger
+          render={
+            <Button
+              size={"icon"}
+              variant={"ghost"}
+              onClick={handleThemeToggle}
+            />
+          }
+        >
+          <IconBrightness />
+        </TooltipTrigger>
+        <TooltipContent>
+          <span>Toggle Theme</span>
+          <Kbd className={"ml-2"}>D</Kbd>
+        </TooltipContent>
+      </Tooltip>
     </>
   );
 }
