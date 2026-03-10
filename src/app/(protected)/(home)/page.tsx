9e2f71b23c8e27c08bc762a207c263c57f12623c
Moe Youssef <moyoussef182005@gmail.com>
2026-03-10 06:28:24 +0200

    Update Next.js configuration and layout; remove default page and add new protected home page
    - Disabled development indicators and enabled typed routes in `next.config.ts`.
    - Updated the title in `layout.tsx` to "Impact GG" and formatted font settings.
    - Removed the default `page.tsx` file.
    - Added a new protected home page component in `src/app/(protected)/(home)/page.tsx` with a simple "Hello World!" message.

---
+++ src/app/(protected)/(home)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <>
+      <div className={"container mx-auto py-4"}>
+        <h1>Hello World!</h1>
+      </div>
+    </>
+  );
+}
